64971b5ff098e7d7976abe40a9dadd88e260d925
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 17:20:36 -0300

    add client interface

--- src/main/java/ibm/elizapimentel/DummyProducts/client/DummyClient.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/client/DummyClient.java
@@ -1,2 +1,18 @@
-package ibm.elizapimentel.DummyProducts.Client;public class DummyClient {
+package ibm.elizapimentel.DummyProducts.client;
+
+import ibm.elizapimentel.DummyProducts.model.dto.Products;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "products", url = "${api.dummy.products.url}")
+public interface DummyClient {
+
+    @GetMapping()
+    Products getAllProducts();
+
 }
+   /* @RequestMapping(method = RequestMethod.GET, value = "/products")
+    List<ProductsDTO> getAllProducts();
+
+    @RequestMapping(method = RequestMethod.POST, value = "/products/{productId}", consumes = "application/json")
+    ProductsDTO update(@PathVariable("productId") Long productId, ProductsDTO store);*/
