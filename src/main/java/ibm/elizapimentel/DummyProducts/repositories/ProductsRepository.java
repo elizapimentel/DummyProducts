265b7616a307d7442f1075172a5dc9cc050c29c0
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 17:27:18 -0300

    add repo (commented)

--- src/main/java/ibm/elizapimentel/DummyProducts/repositories/ProductsRepository.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/repositories/ProductsRepository.java
@@ -1,2 +1,11 @@
-package ibm.elizapimentel.DummyProducts.repositories;public class ProductsRepository {
+/*
+package ibm.elizapimentel.DummyProducts.repositories;
+
+import ibm.elizapimentel.DummyProducts.model.ProductsRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductsRepository extends JpaRepository<ProductsRequest, Long> {
 }
+*/
