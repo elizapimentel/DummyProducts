8b19f999b0de98bb5223d05e06fe71263ca9ba13
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 17:28:33 -0300

    add service impl get all)

--- src/main/java/ibm/elizapimentel/DummyProducts/services/ProductsImpl.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/services/ProductsImpl.java
@@ -1,2 +1,31 @@
-package ibm.elizapimentel.DummyProducts.services;public class ProductsImpl {
+package ibm.elizapimentel.DummyProducts.services;
+
+import ibm.elizapimentel.DummyProducts.client.DummyClient;
+import ibm.elizapimentel.DummyProducts.mapper.ProductsMapper;
+import ibm.elizapimentel.DummyProducts.model.dto.ProductsResponse;
+//import ibm.elizapimentel.DummyProducts.repositories.ProductsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+public class ProductsImpl implements ProductsService{
+    @Autowired
+    private DummyClient client;
+//    @Autowired
+//    private ProductsRepository repo;
+    @Autowired
+    private ProductsMapper mapper;
+
+
+    @Override
+    public List<ProductsResponse> getAllProducts() {
+
+        return client.getAllProducts().getProducts();
+    }
 }
