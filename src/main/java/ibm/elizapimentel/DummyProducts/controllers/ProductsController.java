c8f2c5a562d17ae64b6b1fcd87df0c8a8c4ad1ee
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 17:22:08 -0300

    add get all

--- src/main/java/ibm/elizapimentel/DummyProducts/controllers/ProductsController.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/controllers/ProductsController.java
@@ -1,2 +1,25 @@
-package ibm.elizapimentel.DummyProducts.controllers;public class ProductsController {
+package ibm.elizapimentel.DummyProducts.controllers;
+
+import ibm.elizapimentel.DummyProducts.model.dto.ProductsResponse;
+import ibm.elizapimentel.DummyProducts.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductsService service;
+
+    @GetMapping()
+    public ResponseEntity<List<ProductsResponse>> getAll() {
+        List<ProductsResponse> products = service.getAllProducts();
+        return ResponseEntity.status(200).body(products);
+    }
 }
