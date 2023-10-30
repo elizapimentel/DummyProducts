1d29e95af5343909ef85826f20e49f6fe237944f
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 11:38:00 -0300

    first commit

---
+++ src/main/java/ibm/elizapimentel/DummyProducts/DummyProductsApplication.java
@@ -0,0 +1,13 @@
+package ibm.elizapimentel.DummyProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibm/elizapimentel/DummyProducts/DummyProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.elizapimentel.DummyProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
