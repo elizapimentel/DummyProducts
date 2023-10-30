4819805e3f92e4d6267dad432470e50372a4770c
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 17:24:25 -0300

    add mapstruct (commented)

--- src/main/java/ibm/elizapimentel/DummyProducts/mapper/ProductsMapper.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/mapper/ProductsMapper.java
@@ -1,2 +1,16 @@
-package ibm.elizapimentel.DummyProducts.mapper;public interface ProductsMapper {
+/*
+package ibm.elizapimentel.DummyProducts.mapper;
+
+import ibm.elizapimentel.DummyProducts.model.ProductsRequest;
+import ibm.elizapimentel.DummyProducts.model.dto.ProductsResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper( componentModel = "Spring" )
+public interface ProductsMapper {
+    ProductsMapper MAPPER = Mappers.getMapper(ProductsMapper.class);
+    ProductsResponse modelToDto(ProductsRequest model);
+    ProductsRequest dtoToModel(ProductsResponse dto);
+
 }
+*/
