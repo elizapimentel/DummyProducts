26ebdc6b8c8ea0277de6581a866d9fb645651ff3
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-30 17:19:37 -0300

    enabled feign

--- src/main/java/ibm/elizapimentel/DummyProducts/DummyProductsApplication.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/DummyProductsApplication.java
@@ -2,11 +2,14 @@ package ibm.elizapimentel.DummyProducts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DummyProductsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DummyProductsApplication.class, args);
 	}
 
