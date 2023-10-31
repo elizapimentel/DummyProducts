bd0903c57ba65bfeacef233656d8fbf54648fa13
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-31 10:38:52 -0300

    changed get all with mapper

--- src/main/java/ibm/elizapimentel/DummyProducts/services/ProductsImpl.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/services/ProductsImpl.java
@@ -1,31 +1,34 @@
 package ibm.elizapimentel.DummyProducts.services;
 
 import ibm.elizapimentel.DummyProducts.client.DummyClient;
-//import ibm.elizapimentel.DummyProducts.mapper.ProductsMapper;
+import ibm.elizapimentel.DummyProducts.mapper.ProductsMapper;
+import ibm.elizapimentel.DummyProducts.model.ProductsRequest;
 import ibm.elizapimentel.DummyProducts.model.dto.ProductsResponse;
-//import ibm.elizapimentel.DummyProducts.repositories.ProductsRepository;
+import ibm.elizapimentel.DummyProducts.repositories.ProductsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static java.lang.String.format;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductsImpl implements ProductsService{
     @Autowired
     private DummyClient client;
-//    @Autowired
-//    private ProductsRepository repo;
-//    @Autowired
-//    private ProductsMapper mapper;
+    @Autowired
+    private ProductsRepository repo;
+    @Autowired
+    private ProductsMapper mapper;
 
 
     @Override
-    public List<ProductsResponse> getAllProducts() {
-
-        return client.getAllProducts().getProducts();
+    public List<ProductsRequest> getAllProducts() {
+        List<ProductsResponse> response = client.getAllProducts().getProducts();
+        List<ProductsRequest> requestList = response.stream()
+                .map(ProductsMapper.MAPPER::dtoToModel)
+                .collect(Collectors.toList());
+        return repo.saveAll(requestList);
     }
 }
