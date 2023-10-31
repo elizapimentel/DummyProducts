6be0514f9832c50663949e602a749ab7c4160c0a
eliza-pimentel <eliza.pimentel@ibm.com>
2023-10-31 19:00:23 -0300

    add constants data

---
+++ src/test/java/ibm/elizapimentel/DummyProducts/Common/Constants.java
@@ -0,0 +1,21 @@
+package ibm.elizapimentel.DummyProducts.Common;
+
+import java.util.List;
+
+public class Constants {
+
+    public static final Long ID                       = 1L;
+    public static final String TITLE                  = "iPhone 9";
+    public static final String DESCRIPTION            = "An apple mobile which is nothing like apple";
+    public static final Integer PRICE                 = 549;
+    public static final Double DISCOUNT_PERCENTAGE    = 12.96;
+    public static final Double RATING                 = 4.69;
+    public static final Integer STOCK                 = 94;
+    public static final String BRAND                  = "Apple";
+    public static final String CATEGORY               = "smartphones";
+    public static final String THUMBNAIL              = "https://i.dummyjson.com/data/products/1/thumbnail.jpg";
+    public static final List<String> IMAGES           = List.of("https://i.dummyjson.com/data/products/1/1.jpg", "https://i.dummyjson.com/data/products/1/2.jpg", "https://i.dummyjson.com/data/products/1/3.jpg", "https://i.dummyjson.com/data/products/1/4.jpg", "https://i.dummyjson.com/data/products/1/thumbnail.jpg");
+    public static final Integer TOTAL                 = 100;
+    public static final Integer SKIP                  = 0;
+    public static final Integer LIMIT                 = 30;
+}
---
+++ src/test/java/ibm/elizapimentel/DummyProducts/controllers/ProductsControllerTest.java
@@ -0,0 +1,2 @@
+package ibm.elizapimentel.DummyProducts.controllers;public class ProductsControllerTest {
+}
