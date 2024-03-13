34c086c9c772ce9402bd8a45ec36e47946d5b355
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-13 16:40:46 -0300

    add new mapping

--- src/main/java/ibm/elizapimentel/DummyProducts/mapper/ProductsMapper.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/mapper/ProductsMapper.java
@@ -3,12 +3,20 @@ package ibm.elizapimentel.DummyProducts.mapper;
 import ibm.elizapimentel.DummyProducts.model.ProductsRequest;
 import ibm.elizapimentel.DummyProducts.model.dto.ProductsResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper( componentModel = "Spring" )
 public interface ProductsMapper {
     ProductsMapper MAPPER = Mappers.getMapper(ProductsMapper.class);
+
+    @Mapping(target = "id", ignore = true) // Ignorar a atualização do ID
+    void updateModelFromDto(ProductsResponse dto, @MappingTarget ProductsRequest model);
+
     ProductsResponse modelToDto(ProductsRequest model);
+
+    @Mapping(target = "id", ignore = true)
     ProductsRequest dtoToModel(ProductsResponse dto);
 
 }
