61a9ffa560a9a43121168d11c85ed2b8d830e90b
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-14 12:51:13 -0300

    add updateTotalStock

--- src/main/java/ibm/elizapimentel/DummyProducts/repositories/ProductsRepository.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/repositories/ProductsRepository.java
@@ -2,8 +2,11 @@ package ibm.elizapimentel.DummyProducts.repositories;
 
 import ibm.elizapimentel.DummyProducts.model.ProductsRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +18,8 @@ public interface ProductsRepository extends JpaRepository<ProductsRequest, Long>
 
     Optional<ProductsRequest> findByTitle(String title);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE ProductsRequest p SET p.stock = :newStock WHERE p.id = :id")
+    void updateTotalStock(@Param("id") Long id, int newStock);
 }
