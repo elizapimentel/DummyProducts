ee02a80fa995a430ba6185bd3c51cd1bb65b899f
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-14 12:50:26 -0300

    delete updateDtoFromModel

--- src/main/java/ibm/elizapimentel/DummyProducts/mapper/ProductsMapper.java
+++ src/main/java/ibm/elizapimentel/DummyProducts/mapper/ProductsMapper.java
@@ -14,6 +14,9 @@ public interface ProductsMapper {
     @Mapping(target = "id", ignore = true) // Ignorar a atualização do ID
     void updateModelFromDto(ProductsResponse dto, @MappingTarget ProductsRequest model);
 
+    @Mapping(target = "id", ignore = true) // Ignorar a atualização do ID
+    void updateDtoFromModel(ProductsRequest model, @MappingTarget ProductsResponse dto);
+
     ProductsResponse modelToDto(ProductsRequest model);
 
     @Mapping(target = "id", ignore = true)
